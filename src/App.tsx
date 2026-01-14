56bb5aa43c56b5a32bc244d887dd22bf367bff39
Mamadou DIAGNE <diagne264@gmail.com>
2026-01-14 12:29:39 -0300

    feat: implementa componentes base e estilização da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Home from './pages/home/home'
+import Home from './pages/home/Home'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 	return (
 		<>
-			<Home />
+			<Navbar /> 
+			<Home /> 
+			<Footer />
 		</>
 	)
 }
